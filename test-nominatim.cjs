cb4ed5462abdcf707c15b1f1bcbf8597352d1554
eslamsoud <eslamsoud3110@gmail.com>
2026-06-06 04:07:23 +0300

    حذف

--- test-nominatim.cjs
+++
@@ -1,6 +0,0 @@
-(async () => {
-  const query = 'سوبر ماركت';
-  const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(query)}&limit=10&countrycodes=eg&accept-language=ar`;
-  const response = await fetch(url, { headers: { 'User-Agent': 'MyApp/1.0' } });
-  console.log(JSON.stringify(await response.json(), null, 2));
-})();
