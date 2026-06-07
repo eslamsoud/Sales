384cfc8d2e7a4b983c49fe1f4276703c51c6a6cb
eslamsoud <eslamsoud3110@gmail.com>
2026-06-08 01:19:32 +0300

    حذف

--- test_dns2.cjs
+++
@@ -1,7 +0,0 @@
-const dns = require('dns');
-
-['geo-places.eu-north-1.api.aws', 'places.geo.eu-north-1.amazonaws.com'].forEach(host => {
-  dns.lookup(host, (err, address) => {
-    console.log(host, err ? err.code : address);
-  });
-});
