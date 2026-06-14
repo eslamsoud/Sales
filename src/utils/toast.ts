93d5c4ab06f1703d8422a4bb58a8de457f3b0ac3
eslamsoud <eslamsoud3110@gmail.com>
2026-06-14 22:16:58 +0300

    حذف

--- src/utils/toast.ts
+++
@@ -1,5 +0,0 @@
-export const toastEvent = new EventTarget();
-
-export const showToast = (message: string) => {
-  toastEvent.dispatchEvent(new CustomEvent('show-toast', { detail: message }));
-};
--- test_dns.cjs
+++
@@ -1,7 +0,0 @@
-const dns = require('dns');
-
-['geo-places.eu-north-1.amazonaws.com', 'places.geo.eu-north-1.amazonaws.com', 'places.geo-places.eu-north-1.amazonaws.com'].forEach(host => {
-  dns.lookup(host, (err, address) => {
-    console.log(host, err ? err.code : address);
-  });
-});
